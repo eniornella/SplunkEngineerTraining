7d0548bcbdf2719070a8b65633bdac90ca47d408
Eni Ornella <eni.ornella@yahoo.com>
2024-01-19 19:57:44 -0500

    updates

--- AWS/Terraform/Ansible_AWX/route53.tf
+++ AWS/Terraform/Ansible_AWX/route53.tf
@@ -4,8 +4,8 @@ data "aws_route53_zone" "eni_zone" {
 
 }
 resource "aws_route53_record" "instance_records" {
-  zone_id = data.aws_route53_zone.eni_zone.zone_id
-  name    = "awx.${data.aws_route53_zone.eni_zone.name}"
+  zone_id = data.aws_route53_zone.training_zone.zone_id
+  name    = "awx.${data.aws_route53_zone.training_zone.name}"
   type    = "A"
   ttl     = 60
   records = [aws_instance.ansible.public_ip]
